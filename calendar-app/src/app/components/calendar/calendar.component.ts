c592e48214d4a21fc6e83c6f52bd1591df094f72
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-10-03 09:11:24 +0200

    added calendar component

---
+++ calendar-app/src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CalendarService } from '../services/calendar.service';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+  calendars: any[] = [];
+
+  constructor(private calendarService: CalendarService) { }
+
+  ngOnInit(): void {
+    this.loadCalendars();
+  }
+
+  loadCalendars() {
+    this.calendarService.getCalendars().subscribe(data => {
+      this.calendars = data;
+    });
+  }
+
+  createCalendar(name: string) {
+    this.calendarService.createCalendar({ name }).subscribe(() => {
+      this.loadCalendars(); // Refresh the list after creating
+    });
+  }
+}
