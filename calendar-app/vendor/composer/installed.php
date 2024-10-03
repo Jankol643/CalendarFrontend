2fccc45277d55f61c8798bc6aa2d773a6c7359d8
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-10-03 09:12:28 +0200

    initialised composer

---
+++ calendar-app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '20aed714b0a2a707dada7577a16dfed04cfb9936',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '20aed714b0a2a707dada7577a16dfed04cfb9936',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
