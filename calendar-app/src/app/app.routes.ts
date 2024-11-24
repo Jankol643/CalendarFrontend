41e843fa7d4433cfe2c2468016fc18cc5640115a
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-11-24 08:52:18 +0100

    added authGuard to admin route

--- calendar-app/src/app/app.routes.ts
+++ calendar-app/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'admin', component: AdminComponent },
+    { path: 'admin', component: AdminComponent, canActivate: [authGuard] },
     { path: '', redirectTo: '/login', pathMatch: 'full' } // Redirect to login by default
 ];
