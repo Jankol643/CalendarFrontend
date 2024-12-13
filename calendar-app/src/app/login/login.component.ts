9c48eefadab07fef8f0fe2c2b3d62580770cb76a
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-12-13 13:58:37 +0100

    standardized layout of login and registration

--- calendar-app/src/app/login/login.component.ts
+++ calendar-app/src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 // src/app/components/login/login.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { AuthService } from '../../auth/auth.service';
+import { AuthService } from '../auth/auth.service';
+import { inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
@@ -10,8 +11,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './login.component.scss',
   standalone: true,
   imports: [ReactiveFormsModule, RouterModule, CommonModule]
+  imports: [ReactiveFormsModule, RouterModule, CommonModule]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   form!: FormGroup;
   loading = false;
   submitted = false;
@@ -23,27 +25,20 @@ export class LoginComponent implements OnInit {
     private authService: AuthService
   ) { }
 
-  ngOnInit(): void {
-    this.form = this.formbuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    })
-  }
+  protected loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
 
   // convenience getter for easy access to form fields
   get f() { return this.form.controls; }
 
   onSubmit() {
-    this.submitted = true;
-    if (this.form.valid) {
-      this.authService.login(this.form.value)
-        .subscribe({
-          next: (data: any) => {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value)
+        .subscribe((data: any) => {
+          if (this.authService.isLoggedIn()) {
             this.router.navigate(['/calendar']);
-          },
-          error: (err) => {
-            console.error('Login error:', err);
-            // Optionally show an error message to the user
           }
         });
     }
