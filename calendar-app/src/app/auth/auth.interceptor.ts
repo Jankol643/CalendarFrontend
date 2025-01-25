d7363dfde2736f3340661c16b0c9177e159ac0ec
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2025-01-25 21:14:51 +0100

    added refresh token to interceptor

--- calendar-app/src/app/auth/auth.interceptor.ts
+++ calendar-app/src/app/auth/auth.interceptor.ts
@@ -1,19 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { AuthService } from './auth.service';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = sessionStorage.getItem('token'); // Retrieve the token from local storage
-    sessionStorage.setItem('from Interceptor', 'intercepted');
+    const token = this.authService.getToken();
+    let clonedRequest = req;
+
     if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${token}`) // Attach the token to the request
+      clonedRequest = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
       });
-      return next.handle(cloned); // Pass the cloned request to the next handler
-    } else {
-      return next.handle(req); // If no token, proceed with the original request
     }
+
+    return next.handle(clonedRequest).pipe(
+      catchError(err => {
+        if (err.status === 401) {
+          // Token expired, attempt to refresh
+          return this.authService.refreshToken().pipe(
+            switchMap((response: any) => {
+              if (response && response.status === true) {
+                // Retry the original request with the new token
+                const newToken = response.authorisation.token;
+                this.authService.saveToken(newToken);
+                const retryRequest = req.clone({
+                  setHeaders: {
+                    Authorization: `Bearer ${newToken}`
+                  }
+                });
+                return next.handle(retryRequest);
+              }
+              return of(err); // Handle error if refresh fails
+            })
+          );
+        }
+        return of(err); // Handle other errors
+      })
+    );
   }
 }
