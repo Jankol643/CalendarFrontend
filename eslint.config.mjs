3161326474438c13261b34d03b11db947bbfede6
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2025-04-06 18:35:43 +0200

    added structured eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,57 @@
-import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 
+const baseIgnore = [
+  "**/.*",                  // Ignore all hidden files and folders
+  "**/node_modules/**",     // Ignore the node_modules folder
+  "**/vendor/**",           // Ignore the vendor folder
+  "**/cache/**",            // Ignore the .angular folder in example-app
+  "**/temp.js",
+  "config/*",
+  "*.conf.*"
+];
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-]);
+const baseConfig = {
+  languageOptions: {
+    globals: globals.browser,
+  },
+  plugins: { js },
+  rules: {
+    // Limit maximum depth of nested blocks to 4
+    "max-depth": ["error", 4],
+    // Limit the cyclomatic complexity to 20
+    "complexity": ["error", { "max": 20 }]
+    // Define your other source file rules here
+  },
+};
+
+const testConfig = {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      jasmine: true, // Add jasmine globals for test files
+    },
+  },
+  plugins: { js },
+  rules: {
+    // Example rule for test files
+    "no-unused-expressions": "off",
+    "no-undef": "off"
+    // Add more rules as needed
+  },
+};
+
+export default [
+  js.configs.recommended, // Recommended config applied to all files
+  {
+    ignores: [...baseIgnore]
+  },
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts}"],
+    ...baseConfig,
+  },
+  {
+    files: ["**/*.spec.{js,mjs,cjs,ts}"], // Test files pattern
+    ...testConfig,
+  },
+];
